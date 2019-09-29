7a8a7874347241acbfb616b170e2b8bfeb84e8c7
SunMyeong Lee <actumn814@gmail.com>
2019-09-29 20:44:48 +0900

    extract_destination

--- finite_automata.py
+++ finite_automata.py
@@ -1,14 +1,10 @@
-def final_states(line):
-    final_states_line = list(map(int, line.split(' ')))
-    return final_states_line[0], final_states_line[1:]
-
-
 class NFA:
     def __init__(self, lines):
         self.num_states = int(lines[0])
         self.states = list(range(self.num_states))
         self.symbols = list(lines[1].strip())
-        self.num_accepting_states, self.accepting_states = final_states(lines[2])
+        final_states_line = list(map(int, lines[2].split(' ')))
+        self.num_final_states, self.final_states = final_states_line[0], final_states_line[1:]
         self.start_state = int(lines[3])
 
         self.transition_functions = []
@@ -37,6 +33,17 @@ class NFA:
         return result
 
 
+def extract_destination(nfa_transition_dict, dfa_state, symbol):
+    destinations = []
+
+    for nfa_state in dfa_state:
+        if (nfa_state, symbol) in nfa_transition_dict and \
+                nfa_transition_dict[(nfa_state, symbol)] not in destinations:
+            destinations.append(nfa_transition_dict[(nfa_state, symbol)])
+
+    return destinations
+
+
 def destinations_to_final(destinations):
     if not destinations:
         return [None]
@@ -52,13 +59,12 @@ def destinations_to_final(destinations):
 
 class DFA:
     def __init__(self):
-        self.num_states = 0
         self.symbols = []
-        self.num_accepting_states = 0
-        self.accepting_states = []
         self.start_state = 0
-        self.transition_functions = []
         self.q = []
+        self.transition_functions = []
+        self.num_final_states = 0
+        self.final_states = []
 
     def convert_from_nfa(self, nfa):
         self.symbols = nfa.symbols
@@ -73,44 +79,33 @@ class DFA:
         dfa_transition_dict = {}
         for dfa_state in self.q:
             for symbol in nfa.symbols:
-                # Initial transition
+                # dfa_state count is 1 (ex. (0, ), (1, ))
                 if len(dfa_state) == 1 and (dfa_state[0], symbol) in nfa_transition_dict:
-                    dfa_transition_dict[(dfa_state, symbol)] = nfa_transition_dict[(dfa_state[0], symbol)]
-
-                    if tuple(dfa_transition_dict[(dfa_state, symbol)]) not in self.q:
-                        self.q.append(tuple(dfa_transition_dict[(dfa_state, symbol)]))
-
-                # Convert other nfa transitions
+                    final_destination = nfa_transition_dict[(dfa_state[0], symbol)]
+                # Convert other nfa transitions (ex. (0, 1, 2) (2, 3))
                 else:
-                    destinations = []
-
-                    for nfa_state in dfa_state:
-                        if (nfa_state, symbol) in nfa_transition_dict and \
-                                nfa_transition_dict[(nfa_state, symbol)] not in destinations:
-                            destinations.append(nfa_transition_dict[(nfa_state, symbol)])
-
+                    destinations = extract_destination(nfa_transition_dict, dfa_state, symbol)
                     final_destination = destinations_to_final(destinations)
-                    dfa_transition_dict[(dfa_state, symbol)] = final_destination
 
-                    if tuple(final_destination) not in self.q:
-                        self.q.append(tuple(final_destination))
+                dfa_transition_dict[(dfa_state, symbol)] = final_destination
 
-        # Convert NFA states to DFA states            
-        for key in dfa_transition_dict:
-            self.transition_functions.append(
-                (self.q.index(tuple(key[0])), key[1], self.q.index(tuple(dfa_transition_dict[key]))))
+                if tuple(final_destination) not in self.q:
+                    self.q.append(tuple(final_destination))
+
+        # Convert NFA states to DFA states
+        for key, value in dfa_transition_dict.items():
+            self.transition_functions.append((self.q.index(tuple(key[0])), key[1], self.q.index(tuple(value))))
 
         for q_state in self.q:
-            for nfa_accepting_state in nfa.accepting_states:
-                if nfa_accepting_state in q_state:
-                    self.accepting_states.append(self.q.index(q_state))
-                    self.num_accepting_states += 1
+            for nfa_final_state in nfa.final_states:
+                if nfa_final_state in q_state:
+                    self.final_states.append(self.q.index(q_state))
+                    self.num_final_states += 1
 
     def print(self):
         print(len(self.q))
         print(''.join(self.symbols))
-        print(str(self.num_accepting_states) + ' ' + 
-              ' '.join(str(accepting_state) for accepting_state in self.accepting_states))
+        print(str(self.num_final_states) + ' ' + ' '.join(str(final_state) for final_state in self.final_states))
         print(self.start_state)
 
         for transition in sorted(self.transition_functions):
